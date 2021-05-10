d21758c1aceb6ae4b0c9226cea5d451f8b069c15
Redmar <redmar@ubuntu.com>
2021-05-10 09:02:35 +0200

    Accept VCF as input, add maximum size for variants

--- create-library.py
+++ create-library.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
 import argparse
-import time
-import os
-import logging
 
+from cyvcf2 import VCF
 from Bio import SeqIO
 
-def create_library(name, chrom, begin, flank_size=20, end=None):
+def create_library(name, chrom, begin, end=None, flank_size=20):
     """ Create a library line 
 
     chrom should be a Biopython Seq object of the relevant chromosome
@@ -20,18 +18,26 @@ def create_library(name, chrom, begin, flank_size=20, end=None):
     right_marker = chrom[end : end+flank_size].seq
     expected_allele = chrom[begin:end].seq
 
-    return f'{left_marker}\t{right_marker}\t{expected_allele} 1 1'
+    return f'{name}\t{left_marker}\t{right_marker}\t{expected_allele} 1 1'
 
 def main(args):
-    logging.debug(args)
-
     # Read the reference fasta (requires biopython >=1.52
     record_dict = SeqIO.index(args.reference, 'fasta')
-    # Get the correct chromosome
-    chrom = record_dict[args.chromosome]
 
-    lib = create_library('name', chrom, args.begin, flank_size=args.flank_size)
-    print(lib)
+    # Open the VCF
+    for variant in VCF(args.vcf):
+        name = f'{variant.CHROM}:{variant.POS}'
+        chrom = record_dict[variant.CHROM]
+        # Get the size of the event
+        size = max (len(x) for x in [variant.REF]+variant.ALT)
+        # Skip if the event is too large
+        if size > args.max_size:
+            continue
+        # VCF's are 1-based
+        begin = variant.POS - 1
+        end = begin + size
+        lib = create_library(name, chrom, begin, end, args.flank_size)
+        print(lib)
 
 
 if __name__ == '__main__':
@@ -40,17 +46,12 @@ if __name__ == '__main__':
     )
     parser.add_argument('--reference', required=True,
                         help='Reference fasta file')
-    parser.add_argument('--begin', required=True, type=int,
-                        help='Beginning of region of interest (0 based)')
-    parser.add_argument('--end', required=False, type=int,
-                        help='End of region of interest (0 based)')
-    parser.add_argument('--chromosome', default='chr7',
-                        help='Chromosome for the region of interest')
+    parser.add_argument('--vcf', required=True,
+                        help='Create a library for the variants in the VCF')
     parser.add_argument('--flank-size', default=20, type=int,
                         help='Size of the flanking regions')
+    parser.add_argument('--max-size', default=20, type=int,
+                        help='Maximum size of an indel to verify')
 
     arguments = parser.parse_args()
-    # If no end is specified, 
-    if not arguments.end:
-        arguments.end = arguments.begin + 1
     main(arguments)
